6f6bd5223efba54cec84300f218fbdd4031a7576
hijiri <crew.sento0864@gmail.com>
2025-01-04 00:01:25 +0900

    1/3 header構築

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const hum = document.querySelector('hum');
+const line = document.querySelectorAll('hum-line');
+const guide = document.querySelector('hum-guide');
+
+hum.addEventListener(click,()=>{
+    
+});
