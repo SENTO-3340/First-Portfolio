8fa6bbe8333961b9e987f7fc725c34c8f1a84285
hijiri <crew.sento0864@gmail.com>
2025-01-04 23:20:52 +0900

    妻の編み物ブログにシフトチェンジ

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-const hum = document.querySelector('hum');
-const line = document.querySelectorAll('hum-line');
-const guide = document.querySelector('hum-guide');
-
-hum.addEventListener(click,()=>{
-    
+// ハンバーガーメニューの制御
+const toggleHum = document.querySelector('.hum');
+const toggleText = document.querySelector('.hum-guide');
+const toggleNav = document.querySelector('.header-nav');
+toggleHum.addEventListener('click', () => {
+  toggleHum.classList.toggle('is-active');
+  toggleNav.classList.toggle('is-active');
+  if (toggleText.textContent === 'open') {
+    toggleText.textContent = 'close';
+  } else {
+    toggleText.textContent = 'open';
+  }
 });
