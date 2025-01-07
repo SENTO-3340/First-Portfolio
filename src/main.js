fc96edeccd216621d82178a305d767bc40a06666
hijiri <crew.sento0864@gmail.com>
2025-01-07 22:57:01 +0900

    jsリファクタリング

--- src/main.js
+++ src/main.js
@@ -1,23 +1,10 @@
-// ハンバーガーメニューの制御
-const toggleHum = document.querySelector('.hum');
-const toggleText = document.querySelector('.hum-guide');
-const toggleNav = document.querySelector('.header-nav');
-toggleHum.addEventListener('click', () => {
-  toggleHum.classList.toggle('is-active');
-  toggleNav.classList.toggle('is-active');
-  if (toggleText.textContent === 'open') {
-    toggleText.textContent = 'close';
-  } else {
-    toggleText.textContent = 'open';
-  }
-});
-
-// ナビのリンクをクリックしたらハンバーガーメニューを閉じる制御
-toggleNav.addEventListener('click', () => {
-  toggleHum.classList.remove('is-active');
-  toggleNav.classList.remove('is-active');
-  toggleText.textContent = 'open';
-});
+// 操作用DOM取得
+const mainMove = document.querySelector('.main-move');
+const header = document.querySelector('.header-inner');
+const nav = document.querySelector('.header-nav');
+const humButton = document.querySelector('.hum');
+const humGuide = document.querySelector('.hum-guide');
+const humLine = document.querySelectorAll('.hum-line');
 
 // スクロールに伴うfade-in制御、header文字色制御
 window.addEventListener('scroll', () => {
@@ -33,24 +20,34 @@ window.addEventListener('scroll', () => {
     }
   });
 
-//headerの文字色をスクロールに伴い変化させる制御 
-  const mainMove = document.querySelector('.main-move');
-  const color = {
-    header: document.querySelector('.header-inner'),
-    nav: document.querySelector('.header-nav'),
-    humLine: document.querySelectorAll('.hum-line'),
-  };
-  color.humLine.forEach((line) => {
+  //headerの文字色をスクロールに伴い変化させる制御
+  humLine.forEach((line) => {
     const distanceToMove = mainMove.offsetTop;
     const height = mainMove.offsetHeight;
     if (scrollTop >= distanceToMove + height) {
-      color.header.classList.add('black');
-      color.nav.classList.add('black');
+      header.classList.add('black');
       line.classList.add('black');
     } else {
-      color.header.classList.remove('black');
-      color.nav.classList.remove('black');
+      header.classList.remove('black');
       line.classList.remove('black');
     }
   });
+});
+
+// ハンバーガーメニューの制御
+humButton.addEventListener('click', () => {
+  humButton.classList.toggle('is-active');
+  nav.classList.toggle('is-active');
+  if (humGuide.textContent === 'open') {
+    humGuide.textContent = 'close';
+  } else {
+    humGuide.textContent = 'open';
+  }
+});
+
+// ナビのリンクをクリックしたらハンバーガーメニューを閉じる制御
+nav.addEventListener('click', () => {
+  nav.classList.remove('is-active');
+  nav.classList.remove('is-active');
+  humGuide.textContent = 'open';
 });
